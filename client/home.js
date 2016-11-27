3cb62c3de733eace8f1c2e70c20af9609b5a8596
Hammad Siddiqui <hammadsiddiqui777@gmail.com>
2016-11-27 21:03:30 +0500

    Prototype ready

--- client/home.js
+++ client/home.js
@@ -2,6 +2,33 @@ Template.home.helpers({
     'food' : function () {
         var food = Calories.find({}).fetch();
         return food;
+    },
+    'total': function () {
+        return Session.get('totalCalories');
+    },
+    'consumed' : function () {
+        return Session.get('caloriesConsumed');
+    },
+    'burnt': function () {
+        return Session.get('burnedCalories');
+    },
+    'bodyType' : function () {
+        var totalCalories = Session.get('totalCalories');
+        var consumedCalories = Session.get('caloriesConsumed');
+        var burnedCalories = Session.get('burnedCalories');
+
+        if(consumedCalories <= burnedCalories) {
+            return "fit";
+        }
+        else if(totalCalories < 1500){
+            return "fit"
+        }
+        else if (totalCalories >= 1500){
+            return "unfit";
+        }
+        else {
+            return "fit";
+        }
     }
 });
 
@@ -10,13 +37,56 @@ Template.home.events({
     'click .js-everyday': function (e) {
         e.preventDefault();
         var totalCalories = Session.get('totalCalories');
-        totalCalories = totalCalories - 112;
+        var weight = Session.get('bodyWeight');
+        var workoutCalories = 0;
+        if(weight <= 56){
+            workoutCalories = 180;
+        }
+        else if(weight <= 70 && weight > 56){
+            workoutCalories = 223;
+        }
+        else if(weight <=83 && weight>70){
+            workoutCalories = 266;
+        }
+        else{
+            workoutCalories = 266;
+        }
+        Session.set('burnedCalories', workoutCalories);
+        totalCalories = totalCalories - workoutCalories;
         Session.set('totalCalories', totalCalories);
+    },
+    'click .js-sometimes': function (e) {
+        e.preventDefault();
+        var totalCalories = Session.get('totalCalories');
+        var weight = Session.get('bodyWeight');
+        var workoutCalories = 0;
+        if(weight <= 56){
+            workoutCalories = 90;
+        }
+        else if(weight <= 70 && weight > 56){
+            workoutCalories = 112;
+        }
+        else if(weight <=83 && weight>70){
+            workoutCalories = 133;
+        }
+        else{
+            workoutCalories = 133;
+        }
+
+        Session.set('burnedCalories', workoutCalories);
+        totalCalories = totalCalories - workoutCalories;
+        Session.set('totalCalories', totalCalories);
+    },
+    'click .js-never': function (e) {
+        e.preventDefault();
+        var totalCalories = Session.get('totalCalories');
+        //Laziness adds more fat
+        Session.set('totalCalories', totalCalories + 100);
     },
     'submit .js-weight' : function (e) {
         e.preventDefault();
-        var value = e.target.weight.value;
-        console.log(value);
+        var value = parseInt(e.target.weight.value);
+        Session.set('bodyWeight', value);
     }
 });
 
@@ -27,19 +97,20 @@ Template.home.onRendered(function () {
         $(".chzn-select").chosen({no_results_text: "Oops, nothing found!", width: "350px"});
     }
     $(".chzn-select").on('change', function(evt, params) {
-        console.log(evt);
-        console.log(params);
+
         if(params.selected){
             console.log("Selected");
             var oldCalories = Session.get('totalCalories');
             var newCalories = parseInt(params.selected, 10)
             Session.set('totalCalories', newCalories + oldCalories);
+            Session.set('caloriesConsumed',newCalories + oldCalories);
         }
         else if(params.deselected) {
             console.log("Selected");
             var oldCalories = Session.get('totalCalories');
             var newCalories = parseInt(params.deselected, 10)
             Session.set('totalCalories', oldCalories - newCalories);
+            Session.set('caloriesConsumed',oldCalories - newCalories);
         }
     });
 });
--- client/startup.js
+++ client/startup.js
@@ -1,3 +1,5 @@
 Meteor.startup(function () {
    Session.set('totalCalories', 0);
+   Session.set('caloriesConsumed', 0);
+   Session.set('burnedCalories',0)
 });
